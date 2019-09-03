34ad244e6265959a2a45b2a9e790e74a4d2c1532
Joko Susilo <jksusilo28@gmail.com>
2019-09-03 20:12:26 +0700

    Update logo

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -33,8 +33,7 @@
                 <div class="flex items-center">
                     <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
                         <img class="h-8 md:h-10 mr-3" src="/assets/img/logo.svg" alt="{{ $page->siteName }} logo" />
-
-                        <h1 class="text-lg md:text-2xl text-blue-800 font-semibold hover:text-blue-600 my-0">{{ $page->siteName }}</h1>
+                        {{-- <h1 class="text-lg md:text-2xl text-blue-800 font-semibold hover:text-blue-600 my-0">{{ $page->siteName }}</h1> --}}
                     </a>
                 </div>
 
