40a74c34f9e7c813a5c50e2955c2e12467a6e292
Joko Susilo <jksusilo28@gmail.com>
2020-05-14 23:04:34 +0700

    Add new article

--- source/_components/meta.blade.php
+++ source/_components/meta.blade.php
@@ -3,12 +3,12 @@
 
     <meta property="og:title" content="{{ $title }}" />
     <meta property="og:type" content="website" />
-    <meta property="og:image" content="{{ $page->baseUrl }}/assets/img/logo.svg" />
+    <meta property="og:image" content="{{ $page->baseUrl }}assets/img/logo.svg" />
     <meta property="og:url" content="{{ $url }}"/>
     <meta property="og:description" content="{{ $description }}" />
 
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:title" content="{{ $title }}" />
     <meta name="twitter:description" content="{{ $description }}" />
-    <meta name="twitter:image" content="{{ $page->baseUrl }}/assets/img/logo.svg" />
+    <meta name="twitter:image" content="{{ $page->baseUrl }}assets/img/logo.svg" />
 @endpush
